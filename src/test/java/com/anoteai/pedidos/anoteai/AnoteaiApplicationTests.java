02d2b34b239a3b51b6b677b63c140b92a1f800f1
paulo_sneto <paulo_sneto@yahoo.com>
2024-04-19 14:44:59 -0300

    feat: creating default files

---
+++ src/test/java/com/anoteai/pedidos/anoteai/AnoteaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anoteai.pedidos.anoteai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoteaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
